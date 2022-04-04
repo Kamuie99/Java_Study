43f9f839d0c722b056ba85023148a4591ba2e818
Kamuie99 <101976498+Kamuie99@users.noreply.github.com>
2022-04-04 20:54:27 +0900

    if 조건문 연습 2
    학점 + 0 - 붙여서 A B C

--- practice.java
+++ practice.java
@@ -4,22 +4,32 @@ public class practice {
 	
 	public static void main(String[] args) {
 		int score = 0;
-		char grade = ' ';
+		char grade = ' ', opt = '0';
 		
-		System.out.print("점수를 입력하세요.>");
+		System.out.println("점수를 입력해주세요.");
+		
 		Scanner scanner = new Scanner(System.in);
 		score = scanner.nextInt();
 		
+		System.out.printf("당신의 점수는 %d입니다.", score);
+		
 		if (score >= 90) {
 			grade = 'A';
+			if (score >= 98) {
+				opt = '+';
+			} else if (score < 94) {
+				opt = '-';
+			}
 		} else if (score >= 80) {
 			grade = 'B';
-		} else if (score >= 70) {
-			grade = 'C';
+			if (score >= 88) {
+				opt = '+';
+			} else if (score < 84) {
+				opt = '-';
+			}
 		} else {
-			grade = 'D';
+			grade = 'C';
 		}
-		System.out.println("당신의 학점은 " + grade + "입니다.");
+		System.out.printf("당신의 학점은 %c%c입니다.", grade, opt);
 	}
-	
 }
